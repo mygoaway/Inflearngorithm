490e3fb8456899ced59ce24d8075e67fb6bb27b2
mygoaway2 <mygoaway2@gmail.com>
2021-09-07 23:26:53 +0900

    six commit

---
+++ src/main/java/com/example/algorithm/part1/section6.java
@@ -0,0 +1,27 @@
+package com.example.algorithm.part1;
+
+import java.util.Scanner;
+
+public class section6 {
+    public String solution(String str) {
+        String answer ="";
+
+        for(int i = 0; i < str.length(); i++) {
+            // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            if(str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        section6 se = new section6();
+
+        Scanner scan = new Scanner(System.in);
+        String n = scan.next();
+
+        System.out.println(se.solution(n));
+    }
+}
