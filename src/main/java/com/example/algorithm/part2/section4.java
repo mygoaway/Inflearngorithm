9f3f20f1271acea6da88d2f34b9b0fac736ff7f4
mygoaway2 <mygoaway2@gmail.com>
2021-09-12 15:01:48 +0900

    part2 four commit

---
+++ src/main/java/com/example/algorithm/part2/section4.java
@@ -0,0 +1,29 @@
+package com.example.algorithm.part2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class section4 {
+    public static void main(String[] args) {
+        section4 se3 = new section4();
+        Scanner scan = new Scanner(System.in);
+        int cnt = scan.nextInt();
+
+        for(int x : se3.solution(cnt)) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public int [] solution(int cnt) {
+
+        int [] intArr = new int[cnt];
+        intArr[0] = 1;
+        intArr[1] = 1;
+        for(int i = 2; i < cnt; i++) {
+            intArr[i] = intArr[i-2] + intArr[i-1];
+        }
+
+        return intArr;
+    }
+}
