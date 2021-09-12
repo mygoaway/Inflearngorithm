505d012ecaa8bfe6c49abeccdf4b1dc4e57e3b63
mygoaway2 <mygoaway2@gmail.com>
2021-09-12 14:02:27 +0900

    part2 first commit

---
+++ src/main/java/com/example/algorithm/part2/section1.java
@@ -0,0 +1,34 @@
+package com.example.algorithm.part2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class section1 {
+    public static void main(String[] args) {
+        section1 se1 = new section1();
+        Scanner scan = new Scanner(System.in);
+        int cnt = scan.nextInt();
+        int [] intArr = new int[cnt];
+        for(int i = 0; i < intArr.length; i++) {
+            intArr[i] = scan.nextInt();
+        }
+
+        for(int x : se1.solution(cnt, intArr)){
+            System.out.print(x + " ");
+        }
+    }
+
+    public List<Integer> solution(int cnt, int [] intArr) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(intArr[0]);
+
+        for(int i = 1; i < intArr.length; i++) {
+            if(intArr[i] > intArr[i-1]) {
+                answer.add(intArr[i]);
+            }
+        }
+
+        return answer;
+    }
+}
