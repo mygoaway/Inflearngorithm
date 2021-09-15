0b2d46a66e07798a8f44e73922db6e67c410006a
mygoaway2 <mygoaway2@gmail.com>
2021-09-15 22:58:33 +0900

    part2 seven commit

---
+++ src/main/java/com/example/algorithm/part2/section7.java
@@ -0,0 +1,36 @@
+package com.example.algorithm.part2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class section7 {
+    public static void main(String[] args) {
+        section7 se3 = new section7();
+        Scanner scan = new Scanner(System.in);
+        int cnt = scan.nextInt();
+        int[] intArr = new int[cnt];
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = scan.nextInt();
+        }
+
+        System.out.println(se3.solution(cnt, intArr));
+    }
+
+    public int solution(int cnt, int [] intArr) {
+        int answer = 0;
+        int num = 0;
+
+        for(int i = 0; i < cnt; i++) {
+            if(intArr[i] == 1) {
+                num++;
+                answer += num;
+            } else {
+                num = 0;
+            }
+        }
+        
+        return answer;
+    }
+
+}
