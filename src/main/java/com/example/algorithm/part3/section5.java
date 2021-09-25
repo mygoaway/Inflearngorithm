bef7a8fa4c2568f20faf5a92918f5d1bc88b7d86
mygoaway2 <mygoaway2@gmail.com>
2021-09-25 15:51:57 +0900

    part3 five commit

---
+++ src/main/java/com/example/algorithm/part3/section5.java
@@ -0,0 +1,42 @@
+package com.example.algorithm.part3;
+
+import java.util.Scanner;
+
+public class section5 {
+    public static void main(String[] args) {
+        section5 se1 = new section5();
+        Scanner scan = new Scanner(System.in);
+
+        int cnt = scan.nextInt();
+        System.out.print(se1.solution(cnt));
+
+    }
+
+    public int solution(int cnt) {
+        int answer = 0;
+        int sum = 0;
+        int m = cnt/2 +1;
+
+        int [] intArr = new int [m];
+        for(int i = 0 ; i < intArr.length; i++) {
+            intArr[i] = i+1;
+        }
+
+        int lt = 0;
+        for(int rt = 0; rt < m; rt++) {
+            sum += intArr[rt];
+            if(sum == cnt) {
+                answer++;
+            }
+
+            while(sum >= cnt) {
+                sum -= intArr[lt++];
+                if(sum == cnt) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
