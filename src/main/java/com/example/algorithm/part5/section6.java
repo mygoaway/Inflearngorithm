bf7ffe35b469d9fd07724fca99e4f8807adc7c1f
mygoaway2 <mygoaway2@gmail.com>
2021-10-07 23:45:05 +0900

    part5 six commit

---
+++ src/main/java/com/example/algorithm/part5/section6.java
@@ -0,0 +1,39 @@
+package com.example.algorithm.part5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class section6 {
+    public static void main(String[] args) {
+        section6 se1 = new section6();
+        Scanner scan = new Scanner(System.in);
+
+        int people = scan.nextInt();
+        int cnt = scan.nextInt();
+
+        System.out.print(se1.solution(people, cnt));
+    }
+
+    public int solution(int people, int cnt) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1; i <= people; i++) {
+            q.offer(i);
+        }
+
+        while(!q.isEmpty()) {
+            for(int i = 1; i < cnt; i++) {
+                q.offer(q.poll());
+            }
+            q.poll();
+            if(q.size() == 1){
+                answer = q.poll();
+            }
+        }
+
+        return answer;
+    }
+}
