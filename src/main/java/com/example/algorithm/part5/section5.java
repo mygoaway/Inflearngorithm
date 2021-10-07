0c12f007c12801a3233d4132a595b38aca5667db
mygoaway2 <mygoaway2@gmail.com>
2021-10-07 23:22:43 +0900

    part5 five commit

---
+++ src/main/java/com/example/algorithm/part5/section5.java
@@ -0,0 +1,36 @@
+package com.example.algorithm.part5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class section5 {
+    public static void main(String[] args) {
+        section5 se1 = new section5();
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        System.out.print(se1.solution(str));
+    }
+
+    public int solution(String str) {
+        int answer = 0;
+
+        Stack<Character> st = new Stack();
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '(') {
+                st.push(str.charAt(i));
+            } else {
+                st.pop();
+                if(str.charAt(i-1) == '(') {
+                    answer += st.size();
+                } else {
+                    answer += 1;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+}
