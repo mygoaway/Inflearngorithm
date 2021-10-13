8f904add77f33ccf9f965b73eed60d4dc7f910af
mygoaway2 <mygoaway2@gmail.com>
2021-10-13 23:19:56 +0900

    part6 five commit

---
+++ src/main/java/com/example/algorithm/part6/section5.java
@@ -0,0 +1,32 @@
+package com.example.algorithm.part6;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class section5 {
+    public static void main(String[] args) {
+        section5 se1 = new section5();
+        Scanner scan = new Scanner(System.in);
+
+        int cnt = scan.nextInt();
+        int [] intArr= new int[cnt];
+        for(int i = 0; i < cnt; i++) intArr[i] = scan.nextInt();
+        System.out.print(se1.solution(cnt, intArr));
+    }
+
+    public String solution(int cnt, int [] intArr) {
+        String answer = "U";
+        Arrays.sort(intArr);
+
+        for(int i = 0; i < cnt - 1; i++) {
+            if(intArr[i] == intArr[i+1]) {
+                return "D";
+            }
+        }
+
+        return answer;
+    }
+}
