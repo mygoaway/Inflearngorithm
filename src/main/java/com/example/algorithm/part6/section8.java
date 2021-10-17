5895ddfd4407db698ce3dad75287af465a82c6cc
mygoaway2 <mygoaway2@gmail.com>
2021-10-17 15:31:32 +0900

    part6 section8 commit

---
+++ src/main/java/com/example/algorithm/part6/section8.java
@@ -0,0 +1,47 @@
+package com.example.algorithm.part6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class section8 {
+    public static void main(String[] args) {
+        section8 se1 = new section8();
+        Scanner scan = new Scanner(System.in);
+
+        int cnt = scan.nextInt();
+        int num = scan.nextInt();
+        int [] intArr= new int[cnt];
+        for(int i = 0; i < cnt; i++)
+            intArr[i] = scan.nextInt();
+        System.out.println(se1.solution(cnt, num, intArr));
+    }
+
+    public int solution(int cnt, int num, int [] intArr) {
+        int answer = 0;
+        Arrays.sort(intArr);
+
+        // 이분검색은 정렬이 되어있어야 함!
+        int lt = 0;
+        int rt = cnt - 1;
+
+        while(lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if(intArr[mid] == num) {
+                answer = mid+1;
+                break;
+            }
+            if(intArr[mid] > num) {
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+
+
+
+        return answer;
+    }
+}
