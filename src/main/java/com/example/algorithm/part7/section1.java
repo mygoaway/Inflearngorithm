d21d2dc23bcc9d8e644d1699762e78f53a1e9086
mygoaway2 <mygoaway2@gmail.com>
2021-10-23 12:54:40 +0900

    part7 second commit

--- src/main/java/com/example/algorithm/part7/section1.java
+++ src/main/java/com/example/algorithm/part7/section1.java
@@ -1,7 +1,5 @@
 package com.example.algorithm.part7;
 
-import java.util.Scanner;
-
 // 재귀함수(자기가 자신을 호출) - dfs / recursive
 // 자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지 출력하는 프로그램을 작성하세요.
 public class section1 {
