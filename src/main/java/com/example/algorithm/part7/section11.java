b38a066edecf049255dee8ed4ad77a4604e67e4e
mygoaway2 <mygoaway2@gmail.com>
2021-10-31 22:47:56 +0900

    part7 eleven commit

--- src/main/java/com/example/algorithm/part7/section11.java
+++ src/main/java/com/example/algorithm/part7/section11.java
@@ -4,17 +4,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-
+// 인접 행렬
 public class section11 {
-    static int n, m, answer =0;
+    static int n, m, answer = 0;
     static int [][] graph;
     static int [] ch;
 
     public void DFS(int v) {
-        if(v == n) {
+        if(v == m) {
             answer++;
-        }else {
-            for(int i = 1; i <= n; i++) {
+        } else {
+            for(int i = 1; i <=n; i++) {
                 if(graph[v][i] == 1 && ch[i] == 0) {
                     ch[i] = 1;
                     DFS(i);
@@ -24,10 +24,10 @@ public class section11 {
         }
     }
 
-
     public static void main(String[] args) {
         section11 section11 = new section11();
         Scanner scan = new Scanner(System.in);
+
         n = scan.nextInt();
         m = scan.nextInt();
         graph = new int[n+1][n+1];
