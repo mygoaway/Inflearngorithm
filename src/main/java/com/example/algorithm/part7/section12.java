0fba475a9a6587051fb6870f72122ac1790a0658
mygoaway2 <mygoaway2@gmail.com>
2022-01-24 00:02:42 +0900

    2022-01-24_1

--- src/main/java/com/example/algorithm/part7/section12.java
+++ src/main/java/com/example/algorithm/part7/section12.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 // 경로탐색(인접 리스트)
 public class section12 {
     static int n, m, answer = 0;
-    static ArrayList<ArrayList<Integer>> list;
+    static ArrayList<ArrayList<Integer>> graph;
     static int [] ch;
 
     public void DFS(int v) {
         if(v == n) {
             answer++;
         } else {
-            for(int x : list.get(v)) {
+            for(int x : graph.get(v)) {
                 if(ch[x] == 0) {
                     ch[x] = 1;
                     DFS(x);
@@ -24,27 +24,24 @@ public class section12 {
     }
 
     public static void main(String[] args) {
-        section12 section11 = new section12();
+        section12 section12 = new section12();
         Scanner scan = new Scanner(System.in);
 
         n = scan.nextInt();
         m = scan.nextInt();
-        list = new ArrayList<ArrayList<Integer>>();
+        graph = new ArrayList<>();
         for(int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
+        ch = new int[n+1];
         for(int i = 0; i < m; i++) {
             int a = scan.nextInt();
             int b = scan.nextInt();
-            list.get(a).add(b);
+            graph.get(a).add(b);
         }
-
-
-        ch = new int[n+1];
-
-
         ch[1] = 1;
-        section11.DFS(1);
+        section12.DFS(1);
         System.out.println(answer);
+
     }
 }
